9eec7fb1899134cb5a2de12ec4fc4aa061d6d46b
Skraddd <75692696+Skraddd@users.noreply.github.com>
2026-07-13 07:07:30 +0200

    Update prepare-destiny.bat

--- prepare-destiny.bat
+++ prepare-destiny.bat
@@ -1,12 +1,12 @@
 @echo off
 cd /d "%~dp0web"
-echo === Installazione dipendenze web ===
+echo === Install web dependencies ===
 call yarn install
-echo === Build interfaccia web ===
+echo === Build web interface ===
 call yarn build
 cd /d "%~dp0rift"
-echo === Installazione dipendenze rift ===
+echo === Install rift dependencies ===
 call yarn install
 echo.
-echo Preparazione completata! Ora usa avvia-mimic.bat
+echo Preparation Completed!
 pause
